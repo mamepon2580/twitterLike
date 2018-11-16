d82e443146d321513c7c4756869b89f88be1c71e
Akifumi Takahashi <y_poso_2580@yahoo.co.jp>
2018-11-16 23:21:00 +0900

    view編集

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -1,4 +1,6 @@
 class InfoController < ApplicationController
+  before_action :authenticate_user!  #追加
+  before_action :set_note, only: [:show, :edit, :update, :destroy]
   def index
     @users = User.all
   end

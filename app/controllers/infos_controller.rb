e94e55f0118ccb01481af1229176e0b3fad4c237
Akifumi Takahashi <y_poso_2580@yahoo.co.jp>
2018-11-17 00:52:53 +0900

    一旦セーブ(ページ構成壊れたンゴ)

--- app/controllers/infos_controller.rb
+++ app/controllers/infos_controller.rb
@@ -1,4 +1,4 @@
-class InfoController < ApplicationController
+class InfosController < ApplicationController
   before_action :authenticate_user!  #追加
   before_action :set_note, only: [:show, :edit, :update, :destroy]
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static_pages#index'  #追加
   get 'mypage/index'
-  get 'info/index'
+  get 'infos/index'
 end

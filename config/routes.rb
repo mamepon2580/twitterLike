6f8ac54f1b83e989a82f653a1ef66fb9f1853649
Akifumi Takahashi <y_poso_2580@yahoo.co.jp>
2018-11-17 10:25:23 +0900

    ブランチ切り替える前に保存

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
    sessions: 'users/sessions'
  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'static_pages#index'  #追加
+  root to: 'static_pages#index'
   get 'mypage/index'
   get 'infos/index'
 end

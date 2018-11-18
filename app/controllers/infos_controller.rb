c3846d59f183992915fde45d95d331414bcb1857
Akifumi Takahashi <y_poso_2580@yahoo.co.jp>
2018-11-18 12:16:32 +0900

    relation完成

--- app/controllers/infos_controller.rb
+++ app/controllers/infos_controller.rb
@@ -1,9 +1,12 @@
 class InfosController < ApplicationController
   before_action :authenticate_user!  #追加
   def index
-    @relation = Relation.new
-    @not_follow_users = User.where(id:
-      Relation.where.not(follow_id: current_user.id)
-    )
+    @relation_new = Relation.new
+    @relation_all = Relation.new
+    @not_follow_users =User
+    .where.not(id:current_user.id)
+    .where.not(id:Relation
+      .select("follower_id")
+      .where(follow_id: current_user.id))
   end
 end
--- app/controllers/relations_controller.rb
+++ app/controllers/relations_controller.rb
@@ -1,4 +1,5 @@
 class RelationsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_relation, only: [:show, :edit, :update, :destroy]
 
   # GET /relations
